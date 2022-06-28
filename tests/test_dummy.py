d806e20a8775b75af0437cadf2afce3ff083b8cf
Emil Madsen <sovende@gmail.com>
2022-06-28 14:46:14 +0200

    feat: initial commit

---
+++ tests/test_dummy.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2019-2020 Magenta ApS
+#
+# SPDX-License-Identifier: MPL-2.0
+"""Test nothing."""
+
+
+def test_1() -> None:
+    """This tests nothing."""
+
+
+def test_2() -> None:
+    """This tests nothing."""
+
+
+def test_3() -> None:
+    """This tests nothing."""
+
+
+def test_4() -> None:
+    """This tests nothing."""
